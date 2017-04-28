d52ffcfcba7858c6ee869cd95e84158ac55ff506
brandon <bcooper085@gmail.com>
2017-04-28 12:56:11 -0700

    add crud for newsletter

---
+++ src/SonOfCodSeafood/Controllers/NewsletterController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using SonOfCodSeafood.Models;
+using Microsoft.AspNetCore.Identity;
+
+namespace SonOfCodSeafood.Controllers
+{
+    public class NewsletterController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+        private readonly UserManager<User> _userManager;
+        private readonly SignInManager<User> _signInManager;
+
+        public NewsletterController(UserManager<User> userManager, SignInManager<User> signInManager, ApplicationDbContext db)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+            _db = db;
+        }
+        public IActionResult Index()
+        {
+            return View(_db.Newsletter.ToList());
+        }
+    }
+}
