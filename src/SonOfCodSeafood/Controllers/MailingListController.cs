500eada8f8aab25028d23b2ac7bb7535c175a18f
brandon <bcooper085@gmail.com>
2017-04-28 17:00:45 -0700

    add incomplete project

--- src/SonOfCodSeafood/Controllers/MailingListController.cs
+++ src/SonOfCodSeafood/Controllers/MailingListController.cs
@@ -5,15 +5,18 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using System.Security.Claims;
 using SonOfCodSeafood.Models;
+using Microsoft.AspNetCore.Identity;
 
 namespace SonOfCodSeafood.Controllers
 {
     public class MailingListController : Controller
     {
         private readonly ApplicationDbContext _db;
+        private readonly UserManager<User> _userManager;
 
-        public MailingListController(ApplicationDbContext db)
+        public MailingListController(UserManager<User> userManager, ApplicationDbContext db)
         {
+            _userManager = userManager;
             _db = db;
         }
 
@@ -23,11 +26,19 @@ namespace SonOfCodSeafood.Controllers
         }
 
         [HttpPost]
-        public IActionResult Create(MailingList mailingList)
+        public async Task<IActionResult> Create(MailingList mailingList)
         {
+            var userId = this.User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+            var currentUser = await _userManager.FindByIdAsync(userId);
+            mailingList.User = currentUser;
             _db.MailingLists.Add(mailingList);
             _db.SaveChanges();
-            return RedirectToAction("Index", "Home");
+            return RedirectToAction("Index", "Account");
+        }
+
+        public IActionResult Details()
+        {
+            return View(_db.MailingLists.ToList());
         }
     }
 }
